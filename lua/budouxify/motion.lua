c176cf86892b238f345e665621c12efaa348cf24
atusy <30277794+atusy@users.noreply.github.com>
2025-02-01 12:20:21 +0900

    refactor: make it testable

--- lua/budouxify/motion.lua
+++ lua/budouxify/motion.lua
@@ -31,12 +31,13 @@ function M._find_jump_pos(segments, column, head)
 	return n
 end
 
----W/E motion function
 ---@param head boolean
-local function forward(head)
-	local cursor = vim.api.nvim_win_get_cursor(0)
-	local line = vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], false)[1]
-	local right = line:sub(cursor[2] + 1)
+---@param line string
+---@param col number
+---@param segmenter function
+---@return string | number
+function M._forward(head, line, col, segmenter)
+	local right = line:sub(col + 1)
 
 	local fallback = head and "W" or "E"
 
@@ -49,8 +50,7 @@ local function forward(head)
 			-- jump with W if
 			-- * cursor is on a space
 			-- * cursor is on a sequence of alphanumerics or punctuations followed by a space
-			vim.cmd("normal! " .. fallback)
-			return
+			return fallback
 		end
 
 		if not char:match("%w") and not char:match("%p") then
@@ -61,8 +61,7 @@ local function forward(head)
 
 			-- jump to the next multibyte character
 			-- if cursor is on a sequence of alphanumerics or punctuations followed by a multibyte character
-			vim.api.nvim_win_set_cursor(0, { cursor[1], cursor[2] + i - 1 })
-			return
+			return col + i - 1
 		end
 	end
 
@@ -73,46 +72,65 @@ local function forward(head)
 	-- Example where `|` is the cursor and `^` is the jump target:
 	-- あああa
 	-- |     ^
-	local model = require("budoux").load_japanese_model()
+
 	-- Segmentation requires characters on the left of the cursor.
 	-- TODO:to improve performance, do segmentation within a sentence or between singlebyte characters (%w, %s, %p).
-	local segments = model.parse(line)
-	local n = M._find_jump_pos(segments, cursor[2], head)
+	local segments = segmenter(line)
+	local n = M._find_jump_pos(segments, col, head)
 
 	local w, s, p, r = right:find("%w"), right:find("%s"), right:find("%p"), #right
 	local delta = math.min(w or r, s or r, p or r)
 
 	-- jump to the next segment
-	local nmax = cursor[2] + delta - 1
+	local nmax = col + delta - 1
 	if n < nmax then
-		vim.api.nvim_win_set_cursor(0, { cursor[1], n })
-		return
+		return n
 	end
 
 	-- jump to the next WORD
 	if delta == s then
-		vim.cmd("normal! " .. fallback)
-		return
+		return fallback
 	end
 
 	-- jump to the next %w or %p or the end of the line
-	vim.api.nvim_win_set_cursor(0, { cursor[1], nmax })
+	if delta == w or delta == p then
+		return nmax
+	end
 
 	-- jump to next line if cursor is on the last character
-	vim.cmd("normal! hl") -- correct cursor position if it is on the middle of a multibyte character
-	if vim.api.nvim_win_get_cursor(0)[2] == cursor[2] then
-		vim.cmd("normal! " .. fallback)
+	if delta == #right then
+		if head then
+			return fallback
+		else
+			error("Unimplemented")
+		end
+	end
+	error("Should not be reachable")
+end
+
+---W/E motion function
+---@param opt {head: boolean, win: number?, segmenter: function?}
+function M.forward(opt)
+	opt.win = opt.win or vim.api.nvim_get_current_win()
+	local buf = vim.api.nvim_win_get_buf(opt.win)
+	local cursor = vim.api.nvim_win_get_cursor(opt.win)
+	local line = vim.api.nvim_buf_get_lines(buf, cursor[1] - 1, cursor[1], false)[1]
+	local target = M._forward(opt.head, line, cursor[2], opt.segmenter or require("budoux").load_japanese_model().parse)
+	if type(target) == "string" then
+		vim.cmd("normal! " .. target)
+	else
+		vim.api.nvim_win_set_cursor(opt.win, { cursor[1], target })
 	end
 end
 
 -- あいうえお
 
 function M.W()
-	forward(true)
+	M.forward({ head = true })
 end
 
 function M.E()
-	forward(false)
+	M.forward({ head = false })
 end
 
 pcall(vim.keymap.del, { "n", "x", "o" }, "W")
