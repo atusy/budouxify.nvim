0ccb2294946876e3de96a5ea234992341345caf1
atusy <30277794+atusy@users.noreply.github.com>
2025-03-03 21:54:16 +0900

    feat: skip implement E on the space and next WORD starts with Japanese

--- lua/budouxify/motion.lua
+++ lua/budouxify/motion.lua
@@ -1,5 +1,23 @@
 local M = {}
 
+---@param row number
+---@param head boolean
+---@return { row: number, col: number } | nil
+local function _find_forward_in_next_line(row, head)
+	-- 最終行の行末なら移動しない
+	local lastrow = vim.api.nvim_buf_line_count(0)
+	if row == lastrow then
+		return nil
+	end
+
+	-- 次の行があるなら試す
+	M.find_forward({
+		row = row + 1,
+		col = 0,
+		head = head,
+	})
+end
+
 ---@param opts { row: number?, col: number?, curline: string?, head: boolean }
 ---@return { row: number, col: number } | nil
 M.find_forward = function(opts)
@@ -21,24 +39,19 @@ M.find_forward = function(opts)
 	local rightchars = string.sub(curline, col + 1) -- including the current char
 
 	-- 行末処理
-	if not vim.regex(".."):match_str(rightchars) or string.match(rightchars, "^[%s　]+$") then
-		-- 最終行の行末なら移動しない
-		local lastrow = vim.api.nvim_buf_line_count(0)
-		if row == lastrow then
-			return nil
-		end
-
-		-- 次の行があるなら試す
-		M.find_forward({
-			row = row + 1,
-			col = 0,
-			head = opts.head,
-		})
+	if not vim.regex(".."):match_str(rightchars) then
+		--TODO: implement test
+		return _find_forward_in_next_line(row, opts.head)
 	end
 
 	-- カーソル位置が空白文字
 	local _, prefix_spaces = string.find(rightchars, "^[%s　]+")
 	if prefix_spaces then
+		if string.match(rightchars, "^[%s　]+$") then
+			--TODO: implement test
+			return _find_forward_in_next_line(row, opts.head)
+		end
+
 		-- W
 		if opts.head then
 			return { row = row, col = col + prefix_spaces }
@@ -48,7 +61,17 @@ M.find_forward = function(opts)
 		local _, prefix = string.find(rightchars, "^[%s　]+[%w%p]+")
 		if prefix then
 			return { row = row, col = col + prefix - 1 }
+		else
+			--   あいうえお
+			--   ^cursor
+			error("Unimplemented")
 		end
+	end
+
+	-- カーソル位置が単語文字
+	if string.find(rightchars, "^[%w%p]+[%s　]*$") then
+		--TODO: implement test
+		return _find_forward_in_next_line(row, opts.head)
 	end
 
 	error("Unimplemented")
