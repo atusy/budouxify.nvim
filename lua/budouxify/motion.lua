d94b62c8d520fcab6c491b39ceb1b4248b68b259
atusy <30277794+atusy@users.noreply.github.com>
2025-03-03 09:30:20 +0900

    feat: support cursor on the space

--- lua/budouxify/motion.lua
+++ lua/budouxify/motion.lua
@@ -1,147 +1,46 @@
 local M = {}
 
----@param segments string[]
----@param column number
----@param head boolean
----@return number
-function M._find_jump_pos_from_segments(segments, column, head)
-	local n = 0
-	for _, seg in ipairs(segments) do
-		n = n + #seg
-
-		-- あいうえお_かきくけこ_さしすせそ_たちつてと
-		--                |    E W
-		-- `_`をセグメント協会、`|`をカーソル位置とする。
-		-- nは行頭からカーソル（`|`）を位置を超える最初のセグメントの右端の文字までのバイト数（「あいうえおかきくけこ」）
-		-- nvim_win_set_cursorの列は0-indexedなので、バイト数の合計は実質的に次のセグメントの1文字目の位置で`W`相当になる
-		-- また、最後のセグメントの最後の文字「こ」のバイト数をひくと、「あいうえおかきくけ」のバイト数になり、実質的に「こ」の位置を示すので`E`相当になる
-		if head then
-			if n > column then
-				break
-			end
-		else
-			local segchars = vim.fn.split(seg, [[\zs]])
-			local lastchar = segchars[#segchars]
-			if n - #lastchar > column then
-				n = n - #lastchar
-				break
-			end
-		end
-	end
-	return n
-end
-
----@param head boolean
----@param line string
----@param col number
----@param segmenter function
----@return string | number
-function M._find_jump_pos(head, line, col, segmenter)
-	local right = line:sub(col + 1)
-
-	local fallback = head and "W" or "E"
-
-	--[[ if cursor is on %s, %w, or %p ]]
-	local i = 0
-	for char in right:gmatch(".") do
-		i = i + 1
-
-		if char:match("%s") then
-			-- jump with W if
-			-- * cursor is on a space
-			-- * cursor is on a sequence of alphanumerics or punctuations followed by a space
-			return fallback
-		end
-
-		if not char:match("%w") and not char:match("%p") then
-			if i == 1 then
-				-- jump with budoux if cursor is on a multibyte character
-				break
-			end
-
-			-- jump to the next multibyte character
-			-- if cursor is on a sequence of alphanumerics or punctuations followed by a multibyte character
-			return col + i - 1
-		end
-	end
-
-	-- [[jump with budoux]]
-	-- if there is no break within the sequence of multibyte characters,
-	-- jump to the end of the sequence.
-	--
-	-- Example where `|` is the cursor and `^` is the jump target:
-	-- あああa
-	-- |     ^
-
-	-- Segmentation requires characters on the left of the cursor.
-	-- TODO:to improve performance, do segmentation within a sentence or between singlebyte characters (%w, %s, %p).
-	local segments = segmenter(line)
-	local n = M._find_jump_pos_from_segments(segments, col, head)
-
-	local w, s, p, r = right:find("%w"), right:find("%s"), right:find("%p"), #right
-	local delta = math.min(w or r, s or r, p or r)
-
-	-- jump to the next segment
-	local nmax = col + delta - 1
-	if n < nmax then
-		return n
-	end
-
-	-- jump to the next WORD
-	if delta == s then
-		return fallback
-	end
-
-	-- jump to the next %w or %p or the end of the line
-	if delta == w or delta == p then
-		return nmax
-	end
-
-	-- jump to next line if cursor is on the last character
-	if delta == #right then
-		if head then
-			return fallback
-		else
-			error("Unimplemented")
-		end
-	end
-	error("Should not be reachable")
-end
-
----W/E motion function
----@param opt {head: boolean, win: number?, segmenter: function?}
-function M.forward(opt)
-	opt.win = opt.win or vim.api.nvim_get_current_win()
-	local buf = vim.api.nvim_win_get_buf(opt.win)
-	local cursor = vim.api.nvim_win_get_cursor(opt.win)
-	local line = vim.api.nvim_buf_get_lines(buf, cursor[1] - 1, cursor[1], false)[1]
-	local target =
-		M._find_jump_pos(opt.head, line, cursor[2], opt.segmenter or require("budoux").load_japanese_model().parse)
-	if type(target) == "string" then
-		vim.cmd("normal! " .. target)
+---@param opts { row: number?, col: number?, curline: string?, head: boolean }
+---@return { row: number, col: number } | nil
+M.find_forward = function(opts)
+	local row, col = 0, 0 ---@type number, number
+	if opts.row and opts.col then
+		row, col = opts.row, opts.col
+	elseif not opts.row and not opts.col then
+		local cursor = vim.api.nvim_win_get_cursor(0)
+		row, col = cursor[1], cursor[2]
 	else
-		vim.api.nvim_win_set_cursor(opt.win, { cursor[1], target })
+		error("row and col must be both set or both nil")
 	end
+
+	if row == nil or col == nil then
+		error("row and col should not be nil")
+	end
+
+	local curline = opts.curline or vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+	local rightchars = string.sub(curline, col + 1) -- including the current char
+
+	-- 行末処理
+	if not vim.regex(".."):match_str(rightchars) then
+		-- 最終行の行末なら移動しない
+		local lastrow = vim.api.nvim_buf_line_count(0)
+		if row == lastrow then
+			return nil
+		end
+
+		-- 次の行があるなら試す
+		M.find_forward({ row = row + 1, col = 0, head = opts.head })
+	end
+
+	-- カーソル位置が空白文字
+	local _, prefix_spaces = string.find(rightchars, "^[%s　]+")
+	if prefix_spaces then
+		if opts.head then
+			return { row = row, col = col + prefix_spaces }
+		end
+	end
+
+	error("Unimplemented")
 end
-
--- あいうえお
-
-function M.W()
-	M.forward({ head = true })
-end
-
-function M.E()
-	M.forward({ head = false })
-end
-
-pcall(vim.keymap.del, { "n", "x", "o" }, "W")
-vim.keymap.set({ "n", "x", "o" }, "W", function()
-	M.W()
-end)
-vim.keymap.set({ "n", "x", "o" }, "E", function()
-	M.E()
-end)
--- あなたとジャヴァ今すぐダウンロード
--- W      EW            EW          E
 
 return M
--- tests/budouxify/test_motion.lua
+++ tests/budouxify/test_motion.lua
@@ -4,42 +4,22 @@ local T = MiniTest.new_set()
 
 local M = require("budouxify.motion")
 
-do
-	local base_name = "budouxify.motion._find_jump_pos: "
-	local charlen = #"あ"
-	local segments = {
-		"あああ",
-		"いいい",
-		"ううう",
-	}
-	local heads = { 0 } -- cursor位置なので0始まり
-	for i, segment in ipairs(segments) do
-		table.insert(heads, heads[i] + #segment)
-	end
-
-	T[base_name .. "Wは現在のセグメントの次のセグメントの先頭に移動する"] = function()
-		for idx = 1, #segments - 1 do
-			for col = heads[idx], heads[idx + 1] - 1 do
-				MiniTest.expect.equality(M._find_jump_pos_from_segments(segments, col, true), heads[idx + 1])
-			end
-		end
-	end
-
-	T[base_name .. "カーソル位置がセグメントの末尾より手前の時、Eは現在のセグメントの末尾に移動する"] = function()
-		for idx = 1, #segments - 1 do
-			for col = heads[idx], heads[idx + 1] - charlen - 1 do
-				MiniTest.expect.equality(M._find_jump_pos_from_segments(segments, col, false), heads[idx + 1] - charlen)
-			end
-		end
-	end
-
-	T[base_name .. "カーソル位置がセグメントの末尾の時、Eは次のセグメントの末尾に移動する"] = function()
-		for idx = 1, #segments - 1 do
-			for col = heads[idx + 1] - charlen, heads[idx + 1] - 1 do
-				MiniTest.expect.equality(M._find_jump_pos_from_segments(segments, col, false), heads[idx + 2] - charlen)
-			end
-		end
+T["W: cursor on the space"] = function()
+	local pos = M.find_forward({ row = 1, col = 0, curline = "  　345", head = true })
+	if pos then
+		MiniTest.expect.equality(pos, { row = 1, col = 5 })
+	else
+		MiniTest.expect.equality(pos, nil)
 	end
 end
 
+T["W: cursor on the zenkaku-space"] = function()
+	local spaces = "　 "
+	local pos = M.find_forward({ row = 1, col = 0, curline = spaces .. "abc", head = true })
+	if pos then
+		MiniTest.expect.equality(pos, { row = 1, col = spaces:len() })
+	else
+		MiniTest.expect.equality(pos, nil)
+	end
+end
 return T
