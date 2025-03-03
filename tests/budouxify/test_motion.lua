6403f38c66785b7e563911b1e303b3bd26525bde
atusy <30277794+atusy@users.noreply.github.com>
2025-03-03 21:10:11 +0900

    test: refactor

--- tests/budouxify/test_motion.lua
+++ tests/budouxify/test_motion.lua
@@ -5,37 +5,24 @@ local T = MiniTest.new_set()
 local M = require("budouxify.motion")
 
 T["W: cursor on the space"] = function()
-	local pos = M.find_forward({ row = 1, col = 0, curline = "  　345", head = true })
-	if pos then
-		MiniTest.expect.equality(pos, { row = 1, col = 5 })
-	else
-		MiniTest.expect.equality(pos, nil)
-	end
+	local spaces = "  　"
+	local pos = M.find_forward({ row = 1, col = 0, curline = spaces .. "abc", head = true })
+	MiniTest.expect.equality(pos, { row = 1, col = spaces:len() })
 end
 
 T["W: cursor on the zenkaku-space"] = function()
 	local spaces = "　 "
 	local pos = M.find_forward({ row = 1, col = 0, curline = spaces .. "abc", head = true })
-	if pos then
-		MiniTest.expect.equality(pos, { row = 1, col = spaces:len() })
-	else
-		MiniTest.expect.equality(pos, nil)
-	end
+	MiniTest.expect.equality(pos, { row = 1, col = spaces:len() })
 end
 
 T["E: cursor on the space and next word starts with %w"] = function()
 	local pos = M.find_forward({ row = 1, col = 0, curline = "   abc def", head = false })
-	if not pos then
-		error("pos is nil")
-	end
 	MiniTest.expect.equality(pos, { row = 1, col = 5 })
 end
 
 T["E: cursor on the space and next word starts with %p"] = function()
 	local pos = M.find_forward({ row = 1, col = 0, curline = "   %bc def", head = false })
-	if not pos then
-		error("pos is nil")
-	end
 	MiniTest.expect.equality(pos, { row = 1, col = 5 })
 end
 
