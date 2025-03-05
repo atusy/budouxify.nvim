f16befe454b9b1ede6bf05580633f293945cc911
atusy <30277794+atusy@users.noreply.github.com>
2025-03-05 23:24:28 +0900

    test: refactor

--- tests/budouxify/test_motion.lua
+++ tests/budouxify/test_motion.lua
@@ -8,7 +8,7 @@ T["Cursor virtually on the end of the line"] = function()
 	MiniTest.skip("TODO: This test requires real buffer")
 end
 
-T["Cursor on the space"] = MiniTest.new_set({
+T["Cursor on [%s　]"] = MiniTest.new_set({
 	parametrize = {
 		-- single WORD
 		{ {
@@ -128,7 +128,28 @@ T["Cursor on [%w%p]"] = MiniTest.new_set({
 	},
 })
 
-for _, case in pairs({ "Cursor on [%s　]", "Cursor on [%w%p]" }) do
+T["Cursor on non-ASCII WORD"] = MiniTest.new_set({
+	parametrize = {
+		{ {
+			curline = "今日はGOOD",
+			cursors = "＾　ＥW",
+		} },
+		{ {
+			curline = "今日は天気です。GOOD",
+			cursors = "　　　＾　　　ＥW",
+		} },
+		{ {
+			curline = " 今日は天気です。GOOD",
+			cursors = " 　　　＾　　　ＥW",
+		} },
+		{ {
+			curline = "abc今日は天気です。GOOD",
+			cursors = "   　　　＾　　　ＥW",
+		} },
+	},
+})
+
+for _, case in pairs({ "Cursor on [%s　]", "Cursor on [%w%p]", "Cursor on non-ASCII WORD" }) do
 	for motion, cond in pairs({
 		W = { head = true, regex = "[WＷ]" },
 		E = { head = false, regex = "[EＥ]" },
@@ -144,43 +165,6 @@ for _, case in pairs({ "Cursor on [%s　]", "Cursor on [%w%p]" }) do
 			})
 			MiniTest.expect.equality(given, { row = 1, col = to })
 		end
-	end
-end
-
-T["Cursor on Japanese segment"] = MiniTest.new_set({
-	-- { { "今日は", "天気です。", "GOOD" }, 2 }
-	-- の場合、2番目のセグメントにカーソルを置いたときの挙動をテスト
-	-- 「天」、「気」、「で」、「す」、「。」のそれぞれの位置を試す
-	parametrize = {
-		{ { "今日は", "GOOD" }, 1 },
-		{ { "今日は", "天気です。", "GOOD" }, 2 },
-		{ { " ", "今日は", "天気です。", "GOOD" }, 3 },
-		{ { "abc ", "今日は", "天気です。", "GOOD" }, 3 },
-	},
-})
-
-T["Cursor on Japanese segment"]["W motion"] = function(segments, nth_segment)
-	local line = table.concat(segments)
-	local col_base = 0
-	if nth_segment > 1 then
-		for i = 2, nth_segment do
-			col_base = col_base + #segments[i - 1]
-		end
-	end
-
-	local col = col_base
-	local segmentchars = vim.fn.split(segments[nth_segment], "\\zs")
-	for i, _ in pairs(segmentchars) do
-		if i > 1 then
-			col = col + #segmentchars[i - 1]
-		end
-		local pos = M.find_forward({
-			row = 1,
-			col = col,
-			curline = line,
-			head = true,
-		})
-		MiniTest.expect.equality(pos, { row = 1, col = col_base + #segments[nth_segment] })
 	end
 end
 
